ac839efd833483ca1656e86e561aa070fcda2814
Vladimir <vladimir156xz@gmail.com>
2023-12-09 18:48:44 +0100

    created code for calculate game

---
+++ src/CalculateGame.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace BrainGames\CalculateGame;
+
+function equation()
+{
+    $firstNumber = rand(1, 10);
+    $secondNumber = rand(1, 10);
+    $signs = ["+", "-", "*"];
+    $choosenSign = $signs[rand(0, 2)];
+    $question = "{$firstNumber} {$choosenSign} {$secondNumber}";
+    $correctAnswer = 0;
+    switch ($choosenSign) {
+        case '+':
+            $correctAnswer = $firstNumber + $secondNumber;
+            break;
+        case '-':
+            $correctAnswer = $firstNumber - $secondNumber;
+            break;
+        case '*':
+            $correctAnswer = $firstNumber * $secondNumber;
+            break;
+    }
+    return ["Question" => $question, "Correct" => $correctAnswer];
+}
