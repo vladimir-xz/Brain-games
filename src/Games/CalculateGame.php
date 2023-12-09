b08369cd7ed49bbcd2d58765784b80a8a8262a06
Vladimir <vladimir156xz@gmail.com>
2023-12-09 19:54:11 +0100

    added directory src/Games and corrected namespaces

--- src/Games/CalculateGame.php
+++ src/Games/CalculateGame.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace BrainGames\CalculateGame;
+namespace BrainGames\Games\Calculate;
 
 function equation()
 {
--- src/Games/EvenGame.php
+++ src/Games/EvenGame.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace BrainGames\EvenGame;
+namespace BrainGames\Games\Even;
 
 function askIfEven($playersName)
 {
