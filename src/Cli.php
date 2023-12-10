142c3ffc41c952e7646ab68e6fc1c5c4ed638d29
vladimir-xz <vladimir156xz@gmail.com>
2023-12-10 18:14:39 +0300

    added line Welcome to  the Brain Games!

--- src/Cli.php
+++ src/Cli.php
@@ -7,6 +7,7 @@ use function cli\prompt;
 
 function askForName()
 {
+    line("Welcome to the Brain Games!");
     $name = prompt('May I have your name?');
     line("Hello, %s!", ucfirst($name));
     return $name;
