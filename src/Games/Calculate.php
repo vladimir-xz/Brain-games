33d3e4bd5a2ccdd228d1052fa5c92aa33118ae6c
Vladimir <vladimir156xz@gmail.com>
2023-12-21 21:51:47 +0100

    corrected calculate game variables

--- src/Games/Calculate.php
+++ src/Games/Calculate.php
@@ -12,11 +12,8 @@ function getEquationAndAnswer()
     $firstNumber = rand(1, 10);
     $secondNumber = rand(1, 10);
     $signs = ["+", "-", "*"];
-    $question = "";
-    $correctAnswer = 0;
     $choosenSign = $signs[rand(0, 2)];
-    $numbersAndSign = [$firstNumber, $choosenSign, $secondNumber];
-    $question = implode(' ', $numbersAndSign);
+    $question = implode(' ', [$firstNumber, $choosenSign, $secondNumber]);
     switch ($choosenSign) {
         case '+':
             $correctAnswer = $firstNumber + $secondNumber;
