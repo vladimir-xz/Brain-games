d08e3a6101bdfc52e2ce203277dd2ca2617c26af
Vladimir <vladimir156xz@gmail.com>
2023-12-24 21:35:36 +0100

    added if prime, but do not sure

--- src/Games/Prime.php
+++ src/Games/Prime.php
@@ -7,30 +7,34 @@ use BrainGames\Engine;
 
 use function cli\line;
 
-function getQuestionAndIfPrime()
+function getControlNumbers()
 {
     $number = rand(2, 100);
-    $correctAnswer = "yes";
-    $controlNumbers = [2, 3, 5, 7];
-    if (in_array($number, $controlNumbers, true)) {
-        return ["Question" => $number, "Correct" => $correctAnswer];
-    }
-    foreach ($controlNumbers as $controlNumber) {
-        if ($number % $controlNumber === 0) {
-            $correctAnswer = "no";
-            break;
+    in_array($number, [2, 3, 5, 7], true) ? $correctAnswer = "yes" : $correctAnswer = "no";
+    return ["Question" => $number, "Correct" => $correctAnswer];
+}
+
+function getQuestionAndIfPrime()
+{
+    ["Question" => $number, "Correct" => $correctAnswer] = getControlNumbers();
+    if ($correctAnswer === "no") {
+        $controlNumbers = [2, 3, 5, 7];
+        foreach ($controlNumbers as $controlNumber) {
+            if ($number % $controlNumber === 0) {
+                return ["Question" => $number, "Correct" => $correctAnswer];
+            }
         }
     }
-    return ["Question" => $number, "Correct" => $correctAnswer];
+    return ["Question" => $number, "Correct" => "yes"];
 }
 
 function run()
 {
     $name = Cli\askForName();
-    ["Score" => $gameScore, "Goal" => $gameGoal] = Engine\setGameData();
+    $gameRounds = Engine\getGameRounds();
+    for ($i = 0; $i < $gameRounds; $i++) {
+        $questionsAndAnswers[] = getQuestionAndIfPrime();
+    }
     line("Answer \"yes\" if given number is prime. Otherwise answer \"no\".");
-    do {
-        ["Question" => $question, "Correct" => $correctAnswer] = getQuestionAndIfPrime();
-        $ifContinue = Engine\processGame($name, $question, $correctAnswer, $gameScore, $gameGoal);
-    } while ($ifContinue);
+    Engine\processGame($name, $questionsAndAnswers);
 }
