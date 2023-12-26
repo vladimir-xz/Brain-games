4784a769dbd72fa42eba7b5da0edfb86973f156f
vladimir-xz <vladimir156xz@gmail.com>
2023-12-26 19:04:59 +0300

    switched conditions so first is with command return

--- src/Engine.php
+++ src/Engine.php
@@ -38,12 +38,11 @@ function processGame(string $playerName, array $questionsAndAnswers)
     foreach ($questionsAndAnswers as ["Question" => $question, "Correct" => $correctAnswer]) {
         line("Question: %s", $question);
         $answer = prompt("Your answer is ");
-        if (mb_strtolower($answer) == $correctAnswer) {
-            line("Correct!");
-        } else {
+        if (mb_strtolower($answer) != $correctAnswer) {
             line("'%s' is wrong answer ;(. Correct answer was '%s'.", $answer, $correctAnswer);
             return line("Let's try again, %s!", ucfirst($playerName));
         }
+        line("Correct!");
     }
     line("Congratulations, %s!", ucfirst($playerName));
 }
